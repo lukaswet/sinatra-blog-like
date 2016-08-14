c2a83781830b10e8e455af42448bed86ed422e48
ЛУКАС <lukaswet@gmail.com>
2016-08-14 19:38:37 +0000

    Add comments to posts

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ end
 configure do
     init_db
     @db.execute 'CREATE TABLE IF NOT EXISTS "Posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "created_date" DATE, "content" TEXT)'
+    @db.execute 'CREATE TABLE IF NOT EXISTS "Comments" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "created_date" DATE, "content" TEXT, "post_id" INTEGER)'
 end
 
 get '/' do
@@ -39,4 +40,26 @@ post '/new' do
   @db.execute 'INSERT INTO Posts (content, created_date) VALUES (?, datetime())', [content]
   
   redirect to '/'
+end
+
+get '/details/:post_id' do
+    post_id = params[:post_id]
+    
+    @results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
+    
+    @row = @results[0]
+    
+    @comments = @db.execute 'SELECT * FROM Comments WHERE post_id = ? ORDER BY id', [post_id]
+    
+    erb :details
+end
+
+post '/details/:post_id' do
+    post_id = params[:post_id]
+    
+    content = params[:content]
+    
+    @db.execute 'INSERT INTO Comments (content, created_date, post_id) VALUES (?, datetime(), ?)', [content, post_id]
+    
+    redirect to('/details/' + post_id)
 end
